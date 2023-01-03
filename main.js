2b1ca806486100cf4325218d93628f06c522bc12
ludmilagudino <ludmitrini@gmail.com>
2023-01-03 00:13:00 -0300

    arreglando js

--- main.js
+++ main.js
@@ -11,43 +11,52 @@ check.addEventListener('change', (e) => {
     }
 })
 
-//VALIDACION CONTRASEÑA
+//EXPRESIONES REGULARES PARA VALIDAR LOGIN
 const expresiones = {
-    usuario: /^[a-zA-Z0-9]{1,16}$/,
+    usuario: /^[a-zA-Z0-9]{4,16}$/,
     contraseña: /^\d{4}$/
 
 }
 
-const contraseña = document.getElementById("contraseña");
 
 
-//HABILITAR/DESHABILITAR BOTON LOGIN
+//VALIDACION CAMPOS DE LOGIN
 
 const validarFormulario = (e) => {
     switch (e.target.name) {
         case "usuario":
             if (expresiones.usuario.test(e.target.value)) {
                 document.getElementById("incorrectoUsuario").innerHTML =``
-
+                campos.usuario = true;
+                console.log(campos.usuario);
             }else{
                 document.getElementById("incorrectoUsuario").innerHTML =`
+                <img class="imgError" src="./imagenes/error-solid-24.png" alt="error">
                 <p>Usuario incorrecto </p>
                 `
+                campos.usuario = false;
+                console.log(campos.usuario);
             }
             break;
 
         case "contraseña":
             if (expresiones.contraseña.test(e.target.value)) {
                 document.getElementById("incorrectoContra").innerHTML =``
-
+                campos.contraseña= true;
+                console.log(campos.contraseña);
             }else{
                 document.getElementById("incorrectoContra").innerHTML =`
+                <img class="imgError" src="./imagenes/error-solid-24.png" alt="error">
                 <p>Constraseña incorrecta </p>
                 `
+                campos.contraseña= false;
+                console.log(campos.contraseña);
             }
             break;
     }
 }
+
+//FUNCION PARA VALIDAR LO QUE INGRESA EL USUARIO
 
 const inputs = document.querySelectorAll("#formulario input")
 
@@ -58,7 +67,24 @@ inputs.forEach((input) => {
 
 
 
-login.addEventListener('submit', (e) => {
-    e.preventDefault();
-});
+//BOTON HABILITADO SOLO SI LOS CAMPOS ESTAN VALIDADOS
+
+const campos = {
+    usuario: false,
+    contraseña: false
+}
+
+// login.addEventListener('click', (e) => {
+//     console.log(campos);
+//     e.preventDefault();
+//     if (campos.usuario == true && campos.contraseña == true){
+        
+//     }
+// });
+
+function habilitarBoton (){
+    if (campos.usuario == true && campos.contraseña == true){
+        
+    }
+}
 
